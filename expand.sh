33662ba080278d4b4191cef910dc1d5cf8f0496a
Stas Yakobov <stas@3os.org>
2022-06-25 11:39:09 +0300

    testing 1 2 3

--- expand.sh
+++ expand.sh
@@ -35,8 +35,11 @@ fi
 # VM_ID=$VM_ID
 # EXPAND_BY_GB=$2
 DISK_NAME=$(qm config $VM_ID | grep scsi0: | awk '{split($2,a,":|,");print a[1]}')
+echo $DISK_NAME
 VIRTUAL_DISK_NAME=$(qm config $VM_ID | grep scsi0: | awk '{split($2,a,":|,");print a[2]}')
+echo $VIRTUAL_DISK_NAME
 VIRTUAL_DISK_PATH="/dev/${DISK_NAME}/${VIRTUAL_DISK_NAME}"
+echo $VIRTUAL_DISK_PATH
 
 # Display red warning
 echo VM: ${VM_ID} disk ${DISK_NAME} will be expanded by ${EXPAND_BY_GB}
