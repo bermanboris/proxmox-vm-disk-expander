c3acfc634af67bbe20348f48af78b250ff45636b
Stas Yakobov <16795594+fire1ce@users.noreply.github.com>
2022-06-25 15:29:49 +0300

    typo and line brake fix (#3)

--- expand.sh
+++ expand.sh
@@ -12,29 +12,26 @@ qm list
 while true; do
   # Promt the user to select the vm id to be expanded
   read -p "Enter the VM ID to be expanded: " VM_ID
-  
+
   # Check if the VM id is valid
   if [ -z "$(qm list | grep $VM_ID)" ]; then
-    printf "\nInvalid VM ID"
+    printf "\nInvalid VM ID\n"
   else
     break
   fi
 done
-
 
 while true; do
   # promt the user to enter the size to be expanded
-  read -p "Enter the size to be expanded in GB (exmaple: 10G): " EXPAND_BY_GB
+  read -p "Enter the size to be expanded in GB (example: 10G): " EXPAND_BY_GB
 
   # Check if the size is valid
   if [ -z "$(printf $EXPAND_BY_GB | grep -E '^[0-9]+[G]$')" ]; then
-    printf "\nInvalid size. Please enter the size in GB (exmaple: 10G)"
+    printf "\nInvalid size. Please enter the size in GB (example: 10G)\n"
   else
     break
   fi
 done
-  
-  
 
 # Set needed variables for expansion
 DISK_NAME=$(qm config $VM_ID | grep scsi0: | awk '{split($2,a,":|,");print a[1]}')
@@ -43,7 +40,7 @@ VIRTUAL_DISK_PATH="/dev/${DISK_NAME}/${VIRTUAL_DISK_NAME}"
 
 # Display warning
 echo VM ID ${VM_ID} disk ${DISK_NAME} will be expanded by ${EXPAND_BY_GB}
-echo -e "\e[31mWarning: There is no way to downsize the disk! \e[0m"
+echo -e "\e[31mWarning: There is currently no way to downsize the disk! \e[0m"
 
 # Ask for confirmation "yes" and git enter to continue
 read -p "Are you sure you want to expand the disk? (yes/no): " CONFIRM
