fb86e8e59ebb77f3e86701d30b251e2f5671b96b
Stas Yakobov <stas@3os.org>
2022-06-25 12:11:49 +0300

    testing 1 2 3

--- expand.sh
+++ expand.sh
@@ -6,14 +6,6 @@ if ! [ -x "$(command -v kpartx)" ]; then
   apt-get install -y kpartx
 fi
 
-# # Check if all the arguments are provided
-# if [ $# -lt 2 ]; then
-#   printf "Not enough arguments provided"
-#   printf "Usage: $0 VM_ID EXPAND_BY_GB"
-#   printf "Example: $0 100 20G"
-#   exit 1
-# fi
-
 # List of all the vm
 qm list
 # Promt the user to select the vm id to be expanded
@@ -32,18 +24,14 @@ if [ -z "$(printf $EXPAND_BY_GB | grep -E '^[0-9]+[Gg]$')" ]; then
   exit 1
 fi
 
-# VM_ID=$VM_ID
-# EXPAND_BY_GB=$2
+# Set needed variables
 DISK_NAME=$(qm config $VM_ID | grep scsi0: | awk '{split($2,a,":|,");print a[1]}')
-echo $DISK_NAME
 VIRTUAL_DISK_NAME=$(qm config $VM_ID | grep scsi0: | awk '{split($2,a,":|,");print a[2]}')
-echo $VIRTUAL_DISK_NAME
 VIRTUAL_DISK_PATH="/dev/${DISK_NAME}/${VIRTUAL_DISK_NAME}"
-echo $VIRTUAL_DISK_PATH
 
-# Display red warning
+# Display warning
 echo VM: ${VM_ID} disk ${DISK_NAME} will be expanded by ${EXPAND_BY_GB}
-echo -e "\e[31mWarning: There is no way to downsize the disk \e[0m"
+echo -e "\e[31mWarning: There is no way to downsize the disk! \e[0m"
 
 # Ask for confirmation "yes" and git enter to continue
 read -p "Are you sure you want to expand the disk? (yes/no): " CONFIRM
