656bf6109a058f32e9434c2e389b02868cb16ce5
Stas Yakobov <stas@3os.org>
2022-06-25 10:58:55 +0300

    init

--- expand.sh
+++ expand.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+# Check if all the arguments are provided
 if [ $# -lt 2 ]; then
-    echo "Not enough arguments provided"
-    echo "Usage: $0 VM_ID EXPAND_BY_GB"
-    echo "Example: $0 100 20G"
-    exit 1
+  echo "Not enough arguments provided"
+  echo "Usage: $0 VM_ID EXPAND_BY_GB"
+  echo "Example: $0 100 20G"
+  exit 1
 fi
 
-apt install kpartx -qq > /dev/null
+# Check if kpartx is installed otherwise install kpartx
+if ! [ -x "$(command -v kpartx)" ]; then
+  echo 'Installing kpartx dependency...'
+  sudo apt-get install kpartx
+fi
 
 VM_ID=$1
 NEW_SIZE=$2
@@ -15,11 +20,26 @@ DISK_NAME=$(qm config $1 | grep scsi0: | awk '{split($2,a,":|,");print a[1]}')
 VIRTUAL_DISK_NAME=$(qm config $1 | grep scsi0: | awk '{split($2,a,":|,");print a[2]}')
 VIRTUAL_DISK_PATH="/dev/${DISK_NAME}/${VIRTUAL_DISK_NAME}"
 
-echo VM: ${VM_ID}
-echo DISK: ${DISK_NAME}
-echo NEW SIZE: ${NEW_SIZE}
+# get the current size of the disk
+CURRENT_DISK_SIZE=$(qm list $VM_ID | grep $VIRTUAL_DISK_PATH | awk '{split($2,a,":|,");print a[1]}')
+echo "Current disk size: $CURRENT_DISK_SIZE"
 
-qm resize $VM_ID scsi0 +${NEW_SIZE}
-kpartx -av ${VIRTUAL_DISK_PATH}
-sgdisk ${VIRTUAL_DISK_PATH} -e
-kpartx -d ${VIRTUAL_DISK_PATH}
+# Display red warning
+echo VM: ${VM_ID} disk ${DISK_NAME} will be expanded by ${NEW_SIZE}
+echo -e "\e[31mWarning: There is no way to downsize the disk \e[0m"
+
+# Ask for confirmation
+read -p "Are you sure you want to continue? (yes/no) " -n 1 -r
+
+# if the user says yes, then continue otherwise exit
+if [[ $REPLY =~ ^[yes]$ ]]; then
+  echo "Expanding disk..."
+  sudo qm resize $VM_ID scsi0 +${NEW_SIZE}
+  sudo kpartx -av ${VIRTUAL_DISK_PATH}
+  sudo sgdisk ${VIRTUAL_DISK_PATH} -e
+  sudo kpartx -d ${VIRTUAL_DISK_PATH}
+  echo "Expanding disk...done"
+else
+  echo "Exiting..."
+  exit 1
+fi
