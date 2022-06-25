808a23976fb398cda536b57782b697b2337f47e1
Stas Yakobov <stas@3os.org>
2022-06-25 11:12:00 +0300

    url update

--- expand.sh
+++ expand.sh
@@ -1,17 +1,35 @@
 #!/bin/bash
 
-# Check if all the arguments are provided
-if [ $# -lt 2 ]; then
-  echo "Not enough arguments provided"
-  echo "Usage: $0 VM_ID EXPAND_BY_GB"
-  echo "Example: $0 100 20G"
-  exit 1
-fi
-
 # Check if kpartx is installed otherwise install kpartx
 if ! [ -x "$(command -v kpartx)" ]; then
   echo 'Installing kpartx dependency...'
   sudo apt-get install kpartx
+fi
+
+# # Check if all the arguments are provided
+# if [ $# -lt 2 ]; then
+#   echo "Not enough arguments provided"
+#   echo "Usage: $0 VM_ID EXPAND_BY_GB"
+#   echo "Example: $0 100 20G"
+#   exit 1
+# fi
+
+# List of all the vm
+echo $(qm list)
+# Promt the user to select the vm id to be expanded
+read -p "Enter the VM ID to be expanded: " VM_ID
+# Check if the VM id is valid
+if [ -z "$(qm list | grep $VM_ID)" ]; then
+  echo "Invalid VM ID"
+  exit 1
+fi
+
+# prom the user to enter the size to be expanded
+read -p "Enter the size to be expanded in GB (exmaple: 12g): " EXPAND_BY_GB
+# Check if the size is valid
+if [ -z "$(echo $EXPAND_BY_GB | grep -E '^[0-9]+[Gg]$')" ]; then
+  echo "Invalid size"
+  exit 1
 fi
 
 VM_ID=$1
@@ -19,10 +37,6 @@ NEW_SIZE=$2
 DISK_NAME=$(qm config $1 | grep scsi0: | awk '{split($2,a,":|,");print a[1]}')
 VIRTUAL_DISK_NAME=$(qm config $1 | grep scsi0: | awk '{split($2,a,":|,");print a[2]}')
 VIRTUAL_DISK_PATH="/dev/${DISK_NAME}/${VIRTUAL_DISK_NAME}"
-
-# get the current size of the disk
-CURRENT_DISK_SIZE=$(qm list $VM_ID | grep $VIRTUAL_DISK_PATH | awk '{split($2,a,":|,");print a[1]}')
-echo "Current disk size: $CURRENT_DISK_SIZE"
 
 # Display red warning
 echo VM: ${VM_ID} disk ${DISK_NAME} will be expanded by ${NEW_SIZE}
